1cc49b686d402b7ff541be0d58a243bdf34460c5
euny4 <salmaghanam66@gmail.com>
2026-07-26 21:02:13 +0200

    Add initial product list with details

--- products.js
+++ products.js
@@ -1,1 +1,58 @@
-const products = [];
+const products = [
+{
+    id:1,
+    name:"فستان صيفي أنيق",
+    category:"نساء",
+    price:39,
+    image:"images/women/women1.jpg"
+},
+{
+    id:2,
+    name:"حقيبة جلد فاخرة",
+    category:"حقائب",
+    price:55,
+    image:"images/bags/bag1.jpg"
+},
+{
+    id:3,
+    name:"حذاء رياضي",
+    category:"أحذية",
+    price:79,
+    image:"images/shoes/shoe1.jpg"
+},
+{
+    id:4,
+    name:"قميص رجالي",
+    category:"رجال",
+    price:35,
+    image:"images/men/man1.jpg"
+},
+{
+    id:5,
+    name:"فستان أطفال",
+    category:"أطفال",
+    price:25,
+    image:"images/kids/kid1.jpg"
+},
+{
+    id:6,
+    name:"مجموعة عناية بالبشرة",
+    category:"تجميل",
+    price:42,
+    image:"images/beauty/beauty1.jpg"
+},
+{
+    id:7,
+    name:"سماعات لاسلكية",
+    category:"إلكترونيات",
+    price:69,
+    image:"images/electronics/electronic1.jpg"
+},
+{
+    id:8,
+    name:"ساعة يد",
+    category:"إكسسوارات",
+    price:89,
+    image:"images/accessories/watch1.jpg"
+}
+];
